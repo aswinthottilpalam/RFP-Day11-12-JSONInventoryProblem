1490d47b7d760a46840d70b9f09fa9aa298b27f4
aswin26498 <85921826+aswin26498@users.noreply.github.com>
2022-03-09 17:31:21 +0530

    JSON Inventory management problem

---
+++ JSONinventoryManagement/IncredientsClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSONinventoryManagement
+{
+    public class RiceClass
+    {
+        public String Name { get; set; }
+        public double Weight { get; set; }
+        public double PricePerKg { get; set; }
+    }
+
+    public class PulsesClass
+    {
+        public String Name { get; set; }
+        public double Weight { get; set; }
+        public double PricePerKg { get; set; }
+    }
+
+    public class WheatClass
+    {
+        public String Name { get; set; }
+        public double Weight { get; set; }
+        public double PricePerKg { get; set; }
+    }
+
+    public class InventoryModel
+    {
+        public List<RiceClass> RiceList { get; set; }
+        public List<WheatClass> WheatList { get; set; }
+        public List<PulsesClass> PulsesList { get; set; }
+    }
+}
---
+++ JSONinventoryManagement/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using JSONinventoryManagement;
+
+namespace JSONinventoryManagement
+{
+    internal class Program
+    {
+        const string INVENTORY_JSON = @"E:\desktop\newpgm\JSONinventoryManagement\JSONinventoryManagement\inventory.json";
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(" Welcome to Json Inventory management program ");
+            InventoryMain inv = new InventoryMain();
+            inv.ShowOptions(INVENTORY_JSON);
+        }
+    }
+}
